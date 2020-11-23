4e22b70f795255fb6f3171076696315839ebc961
Matster07 <matveystepin07@gmail.com>
2020-11-23 12:26:52 +0300

    version 1.0.0

--- main.c
+++ main.c
@@ -27,31 +27,75 @@ void initArr(int intArr[], int sizeArr) {
     }
 }
 
-void sort(int intArr[], int sizeArr) {
-    int period;
-    int dop;
+int getNumIncArrAndInitIncArr(int increment[], int sizeArr) {
+    int p1, p2, p3, i;
 
-    if (sizeArr % 2 == 0) {
-        period = sizeArr/2;
-        printf("\n%d\n", period);
+    p1 = p2 = p3 = 1;
 
-        printArr(intArr, sizeArr);
+    if (sizeArr > 0) {
+        for (int i = 0; i < sizeArr; i++) {
+            if (i % 2) {
+                increment[i] = 8*p1 - 6*p2 + 1;
+            } else {
+                increment[i] = 9*p1 - 9*p3 + 1;
+                p2 *= 2;
+                p3 *= 2;
+            }
 
-//        for (int i = 0; i < period; i++) {
-//            dop = intArr[i + period];
-//
-//            if (intArr[i] > intArr[i + period]) {
-//                intArr[i + period] = intArr[i];
-//                intArr[i] = dop;
-//            }
-//        }
+            p1 *= 2;
 
-        printArr(intArr, sizeArr);
-
+            if (3*increment[i] > sizeArr) {
+                increment[i] = 0;
+                return i;
+            }
+        }
     } else {
-        printf("Size must be even!");
+        return 0;
+    }
+}
+
+void sort(int intArr[], int sizeArr) {
+    int increment[20], inc, numInc, comp, j;
+
+    numInc = getNumIncArrAndInitIncArr(increment, sizeArr);
+
+    printArr(intArr, sizeArr);
+    while (numInc > 0) {
+        inc = increment[numInc - 1];
+
+        for (int i = inc; i < sizeArr; i++) {
+            int comp = intArr[i];
+
+            for (j = i - inc; (j >= 0) &&( intArr[i] < intArr[j]); j -= inc) {
+                intArr[j + inc] = intArr[j];
+            }
+
+            intArr[j + inc] = comp;
+        }
+
+        numInc--;
     }
 
+    insertSort(intArr, sizeArr);
+
+    printArr(intArr, sizeArr);
+}
+
+void insertSort(int intArr[], int sizeArr)
+{
+    int newElement, index;
+
+    for (int i = 1; i < sizeArr; i++)
+    {
+        newElement = intArr[i];
+        index = i - 1;
+        while(index >= 0 && intArr[index] > newElement)
+        {
+            intArr[index+1] = intArr[index];
+            index = index - 1;
+        }
+        intArr[index+1] = newElement;
+    }
 }
 
 void printArr(int array[], int sizeArr) {
